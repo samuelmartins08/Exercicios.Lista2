62b472ef1b43b933ea4d67e052e3818f13cd988f
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-05-14 21:44:32 -0300

    Add files via upload

---
+++ lista2.ex1/lista2.ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o primeiro valor: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite o segundo valor : ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+
+                Console.WriteLine("O número: {0} é o maior", num1);
+
+            }
+            else
+            {
+
+                Console.WriteLine("O número: {0} é o maior", num2);
+            }
+            
+        }
+    }
+}
