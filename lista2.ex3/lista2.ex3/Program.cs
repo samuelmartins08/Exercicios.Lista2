3b404f6086c8ceca57540018b0aca67267d706b2
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-05-14 22:36:30 -0300

    Add files via upload

---
+++ lista2.ex3/lista2.ex3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2.ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o primeiro número: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o terceiro número: ");
+            double num3 = double.Parse(Console.ReadLine());
+
+            if (num1 == num2 && num1 == num3)
+            {
+                Console.WriteLine("Os valores são iguais");
+
+            }
+            else {
+                if (num1 > num2 && num1 > num3)
+                {
+                    Console.WriteLine("O primeiro valor é o maior");
+                } 
+                if (num3 > num1 && num3 > num2)
+                {
+                    Console.WriteLine("O terceiro valor é o maior");
+                }
+                if (num2 > num1 &&  num2 > num3)
+                {
+
+                    Console.WriteLine("O segundo valor é o maior");
+                }
+            }
+           
+
+            
+
+
+        }
+    }
+}
---
+++ lista2.ex3/lista2.ex3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("lista2.ex3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lista2.ex3")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("fcae437e-3238-4cc8-9afc-853ab2318629")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
